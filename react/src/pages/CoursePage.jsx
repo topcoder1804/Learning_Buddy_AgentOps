94290d9cdca59800821dab2e92b479e56beea86a
Govind-Jangra <govind70821@gmail.com>
2025-05-20 15:02:07 +0530

    chore: clean up Resources tab button code in CoursePage

--- react/src/pages/CoursePage.jsx
+++ react/src/pages/CoursePage.jsx
@@ -366,17 +366,7 @@ function CoursePage() {
             >
               Assignments
             </button>
-            {/* RESOURCES TAB ADDED HERE */}
-            <button
-              onClick={() => setActiveTab("resources")}
-              className={`px-4 py-2 font-medium text-sm ${
-                activeTab === "resources"
-                  ? "border-b-2 border-blue-500 text-blue-500"
-                  : "text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
-              }`}
-            >
-              Resources
-            </button>
+
             {/* RESOURCES TAB ADDED HERE */}
             <button
               onClick={() => setActiveTab("resources")}
